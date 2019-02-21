7117e206e30172e4bc248e1b98f043fc9ca23760
Yosshua Cisneros <42878951+YOSSHUA@users.noreply.github.com>
2019-02-21 01:45:53 -0600

    Casi listo

--- CalculadoraEDI/src/libs/ProcesadorDeExpresiones.java
+++ CalculadoraEDI/src/libs/ProcesadorDeExpresiones.java
@@ -1,117 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package libs;
-
-/**
- *
- * @author priet
- */
-public class ProcesadorDeExpresiones {
-    private String expresion;
-    public ProcesadorDeExpresiones() {
-    
-    }
-    
-    private int getJerarquia(char a){
-        int res = 1;
-        if(a == '^')
-            res = 3;
-        else if(a == '*' || a == '/')
-            res = 2;
-        else if(a == ')' || a == '(')
-            res = 0;            
-        return res;
-    }
-    
-    private int compararOperador(char a, char b){
-        return getJerarquia(a)-getJerarquia(b);
-    }
-    
-    private double procesarPostfija(Pila<ElementoDeExpresion> postfija){
-        Pila<Double> valores=new PilaA();
-        
-        while(!postfija.isEmpty()){
-            ElementoDeExpresion e = postfija.pop();
-            if(e instanceof Numero)
-                valores.push(e.getDoubleValue());
-            else{
-                double m=0.0;
-                double b=valores.pop();
-                double a=valores.pop();
-                switch(e.getCharValue()){
-                    case '+':
-                        m = a + b;
-                        break;
-                    case '-':
-                        m = a - b;
-                        break;
-                    case '/':
-                        m = a / b;
-                        break;
-                    case '*':
-                        m = a * b;
-                        break;
-                    case '^':
-                        m = Math.pow(a, b);
-                        break;
-                        
-                }
-                valores.push(m);
-            }
-        }
-        return valores.peek();
-    }
-    private  PilaA<ElementoDeExpresion> convertirPostfija(String cad){
-        PilaA<ElementoDeExpresion> op, resp;
-        int n, j;       
+private  PilaA<ElementoDeExpresion> convertirPostfija(String cad){
+        PilaA<ElementoDeExpresion> op, resp;        
+        int n, j, i;       
         char a;        
-        String num;
+        String num;        
         n = cad.length();                       
         resp = new PilaA<ElementoDeExpresion>();
         op = new PilaA<ElementoDeExpresion>();                
-        for(int i = 0; i < n; i++){
+        cad = cad.replace("-(", "-1*(");    
+        i=0;
+        if(cad.charAt(0) == '-'){ /// Esto es para el caso especial -555....
+                num = obtenerNum(cad,1,n);
+                resp.push(new Numero(-Double.parseDouble(num)));
+                i = num.length() + 1;
+        }
+        while( i < n){
             a = cad.charAt(i);            
-            if(a == '('  || (op.isEmpty() && !isNumber(a))){
+            if(a == '('){
                 op.push(new Operador(a));                
-            }else if(isNumber(a)){
-                j = i;
-                num = "";
-                while(j < n && (isNumber(cad.charAt(j)) || cad.charAt(j) == '.')){
-                    num+=cad.charAt(j);
-                    j++;                    
-                }
+            }else if(esNumero(a)){                                           
+                num = obtenerNum(cad,i,n);
                 resp.push(new Numero(Double.parseDouble(num)));
-                i = j-1;
+                i = i + num.length()-1;
             }else if(a == ')'){
                 while(op.peek().getCharValue() != '('){
-                    resp.push(op.pop());
+                    resp.push(new Operador(op.pop().getCharValue()));  /// Error aqui 
                 }
                 op.pop();
             }else{
-                if( getJerarquia(a) > getJerarquia(op.peek().getCharValue()))
+                if(a == '-' && cad.charAt(i-1) == '('){ // Caso 2*(-2)
+                    num = obtenerNum(cad,i,n);
+                    resp.push(new Numero(-Double.parseDouble(num)));
+                    i = i + num.length();                                        
+                }
+                else if(op.isEmpty())
                     op.push(new Operador(a));
+                else if( getJerarquia(a) > getJerarquia(op.peek().getCharValue()))
+                        op.push(new Operador(a));
                 else{
-                    while(!op.isEmpty() && getJerarquia(a) <= getJerarquia(op.peek().getCharValue())){
+                    while(!op.isEmpty() && getJerarquia(a) <= getJerarquia(op.peek().getCharValue()) && op.peek().getCharValue() != '('){
                         resp.push(op.pop());
                     }
                     op.push(new Operador(a));
-                }
-                    
+                }                    
             }
+            i++;
         }        
         if(!op.isEmpty()){
             while(!op.isEmpty())
                 resp.push(op.pop());
         }
+        
+            /*while(!resp.isEmpty())
+                op.push(resp.pop());        */
         return resp;
-    }  
-    private boolean isNumber(Character a){
+    }   
+    public boolean esNumero(Character a){
         if((int)a >= 48 && (int)a <= 57)
             return true;
         return false;
     }
     
-}
+    public String obtenerNum(String a, int j, int n){
+        String resp;
+        resp = "";
+        while(j < n && (esNumero(a.charAt(j)) || a.charAt(j) == '.')){
+                    resp+=a.charAt(j);
+                    j++;                    
+        }
+        return resp;
+                
+    }
